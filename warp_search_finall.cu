2acab31f783e545d2c38fa7cf4d273492674b53d
xy <1024388662@qq.com>
2024-06-21 07:44:05 +0000

    performance optimization

--- warp_search_finall.cu
+++ warp_search_finall.cu
@@ -1,4 +1,5 @@
 #include<vector>
+#include <numeric>
 #include<fstream>
 #include"config.h"
 #include<stdio.h>
@@ -7,6 +8,8 @@
 #include<cuda_runtime.h>
 #include <memory>
 #include"cublas_v2.h"
+#include"fixhash.h"
+
 
 #include"smmh2.h"
 #include"bin_heap.h"
@@ -23,15 +26,18 @@
 
 #define FULL_MASK 0xffffffff
 #define N_THREAD_IN_WARP 32
-#define N_MULTIQUERY 1
+#define N_MULTIQUERY 16
 #define CRITICAL_STEP 32
 #define N_MULTIPROBE 1
 #define FINISH_CNT 1
-#define N_THREAD_IN_BLOCK 32
+#define N_THREAD_IN_BLOCK 512
 //#define __HASH_TEST
-#define BLOOM_FILTER_BIT64 4000
+#define BLOOM_FILTER_BIT64 6000
 #define BLOOM_FILTER_BIT_SHIFT 3
-#define BLOOM_FILTER_NUM_HASH 8
+#define BLOOM_FILTER_NUM_HASH 5
+
+// #define __ENABLE_VISITED_DEL
+
 
 const int num_vertices = 1000000;
 const int dim = 128;
@@ -40,10 +46,10 @@ const int num_queries = 10000;
 const int degree = 64;
 const int k = 256;
 
-#define TOPK 100
-#define QUEUE_SIZE 100
+#define TOPK 50
+#define QUEUE_SIZE 50
 
-
+#define HASH_TABLE_CAPACITY (TOPK*4*2)
 
 
 template<class A,class B>
@@ -98,34 +104,45 @@ __device__ void computePQTable(
 	
 }
 
+__shared__ KernelPair<dist_t,idx_t> q[N_MULTIQUERY][QUEUE_SIZE + 2];
+__shared__ KernelPair<dist_t,idx_t> topk[N_MULTIQUERY][TOPK + 1];
+__shared__ value_t dist_list[N_MULTIQUERY][FIXED_DEGREE * N_MULTIPROBE];
+// __shared__ int hashdata[N_MULTIQUERY*HASH_TABLE_CAPACITY];
+
 __global__
-void warp_independent_search_kernel(pq_idx_t* d_data,value_t* d_query,idx_t* d_result,graph_node<dim,degree>* d_graph,\
-	pq_value_t* d_pq_centroid,pq_value_t* d_pq_table,value_t* d_datadist_list,BLOOMFILTER_DATA_T* d_hashdata, int num_query){
-    int bid = (blockIdx.x * N_MULTIQUERY) % 10000;
+void warp_independent_search_kernel(value_t* d_data,value_t* d_query,idx_t* d_result,graph_node<dim,degree>* d_graph,\
+	pq_value_t* d_pq_centroid,pq_value_t* d_pq_table,value_t* d_datadist_list,BLOOMFILTER_DATA_T* d_hashdata,int num_query){
+
+	int bid = (blockIdx.x * N_MULTIQUERY) % 10000;
 	const int step = N_THREAD_IN_WARP;
     int tid = threadIdx.x;
-	int cid = tid / CRITICAL_STEP;
-	int subtid = tid % CRITICAL_STEP;
+	int cid = tid / CRITICAL_STEP;	//warp id
+	int subtid = tid % CRITICAL_STEP;	//lane id
 
-#ifndef __ENABLE_VISITED_DEL
-#define HASH_TABLE_CAPACITY (TOPK*4*16)
-#else
-#define HASH_TABLE_CAPACITY (TOPK*4*2)
-#endif
+// #ifndef __ENABLE_VISITED_DEL
+// #define HASH_TABLE_CAPACITY (TOPK*4*16)
+// #else
+// #define HASH_TABLE_CAPACITY (TOPK*4*2)
+// #endif
 
-#ifdef __DISABLE_SELECT_INSERT
-#undef HASH_TABLE_CAPACITY
-#define HASH_TABLE_CAPACITY (TOPK*4*16+500)
-#endif
+
+// #ifdef __DISABLE_SELECT_INSERT
+// #undef HASH_TABLE_CAPACITY
+// #define HASH_TABLE_CAPACITY (TOPK*4*16+500)
+// #endif
 
 #ifdef __HASH_TEST
 	VanillaList* pbf;
 #else
     BlockedBloomFilter<BLOOM_FILTER_BIT64,BLOOM_FILTER_BIT_SHIFT,BLOOM_FILTER_NUM_HASH>* pbf;
+	// FixHash<int,HASH_TABLE_CAPACITY>* pbf;
+
 #endif
-    KernelPair<dist_t,idx_t>* q;
-    KernelPair<dist_t,idx_t>* topk;
-	value_t* dist_list=d_datadist_list+(bid+cid) * FIXED_DEGREE * N_MULTIPROBE;
+    // KernelPair<dist_t,idx_t>* q=d_q_topk+(bid+cid) * (QUEUE_SIZE + 2 + TOPK + 1);
+    // KernelPair<dist_t,idx_t>* topk=d_q_topk+(bid+cid) * (QUEUE_SIZE + 2 + TOPK + 1)+QUEUE_SIZE + 2;
+	// value_t* dist_list=d_datadist_list+(bid+cid) * (FIXED_DEGREE * N_MULTIPROBE);
+
+
 
 	// dist_list = new value_t[FIXED_DEGREE * N_MULTIPROBE];
 	// q= new KernelPair<dist_t,idx_t>[QUEUE_SIZE + 2];
@@ -138,12 +155,14 @@ void warp_independent_search_kernel(pq_idx_t* d_data,value_t* d_query,idx_t* d_r
 		//dist_list = new value_t[FIXED_DEGREE * N_MULTIPROBE];
 		// value_t dist_list2[FIXED_DEGREE * N_MULTIPROBE];
 		// dist_list=dist_list2;
-		q = new KernelPair<dist_t,idx_t>[QUEUE_SIZE + 2];
-		topk = new KernelPair<dist_t,idx_t>[TOPK + 1];
+		// q = new KernelPair<dist_t,idx_t>[QUEUE_SIZE + 2];
+		// topk = new KernelPair<dist_t,idx_t>[TOPK + 1];
 #ifdef __HASH_TEST
 		pbf = new VanillaList();
 #else
     	pbf = new BlockedBloomFilter<BLOOM_FILTER_BIT64,BLOOM_FILTER_BIT_SHIFT,BLOOM_FILTER_NUM_HASH>(d_hashdata +(bid+cid)*BLOOM_FILTER_BIT64 * 2);
+		// pbf = new FixHash<int,HASH_TABLE_CAPACITY>(reinterpret_cast<int*>(d_hashdata) +(bid+cid)*HASH_TABLE_CAPACITY);
+		// pbf = new FixHash<int,HASH_TABLE_CAPACITY>(hashdata +(cid)*HASH_TABLE_CAPACITY);
 #endif
 
 	//pbf = new VanillaList();
@@ -155,20 +174,22 @@ void warp_independent_search_kernel(pq_idx_t* d_data,value_t* d_query,idx_t* d_r
 	__shared__ int finished[N_MULTIQUERY];
 	__shared__ idx_t index_list[N_MULTIQUERY][FIXED_DEGREE * N_MULTIPROBE];
 	__shared__ int index_list_len[N_MULTIQUERY];
+	__shared__ value_t query_point[N_MULTIQUERY][dim];
 	// __shared__ pq_value_t pq_table[N_MULTIQUERY][pq_dim][k];
-	pq_value_t (*pq_table)[pq_dim][k] = (pq_value_t (*)[pq_dim][k])d_pq_table;
+	// pq_value_t (*pq_table)[pq_dim][k] = (pq_value_t (*)[pq_dim][k])d_pq_table;
 	
 	value_t start_distance;
-	__syncthreads();
+	__syncwarp();
 
-	computePQTable(d_pq_centroid,d_query+(bid+cid)*dim,&pq_table[bid+cid][0][0],pq_dim,k,dim,subtid);
-	__syncthreads();
+	//computePQTable(d_pq_centroid,d_query+(bid+cid)*dim,&pq_table[bid+cid][0][0],pq_dim,k,dim,subtid);
 
 	value_t tmp[N_MULTIQUERY];
 	int j=cid;
 	tmp[j] = 0;
 	for(int i = subtid;i < dim;i += step){
-		tmp[j] += (pq_table[bid+j][i][d_data[i]]); 
+		query_point[j][i] = d_query[(bid + j) * dim + i];
+		tmp[j] += (query_point[j][i] - d_data[i]) * (query_point[j][i] - d_data[i]); 
+		// tmp[j] += (pq_table[bid+j][i][d_data[i]]); 
 	}
 	// if(bid==1)
 	// 	printf("tid:%d,sum:%f\t",tid,tmp[j]);
@@ -178,7 +199,7 @@ void warp_independent_search_kernel(pq_idx_t* d_data,value_t* d_query,idx_t* d_r
 	if(subtid == 0){
 		start_distance = tmp[cid];
 	}
-	__syncthreads();
+	__syncwarp();
 	
 	if(subtid == 0){
     	heap_size[cid] = 1;
@@ -188,155 +209,168 @@ void warp_independent_search_kernel(pq_idx_t* d_data,value_t* d_query,idx_t* d_r
 		KernelPair<dist_t,idx_t> kp;
 		kp.first = d;
 		kp.second = 0;
-		smmh2::insert(q,heap_size[cid],kp);
+		smmh2::insert(q[cid],heap_size[cid],kp);
 		pbf->add(0);
 	}
-	__syncthreads();
-	int step_num;
-	step_num=0;
+	__syncwarp();
+	// int step_num;
+	// step_num=0;
     while(heap_size[cid] > 1){
-		step_num++;
+		// step_num++;
 		index_list_len[cid] = 0;
 		int current_heap_elements = heap_size[cid] - 1;
 		for(int k = 0;k < N_MULTIPROBE && k < current_heap_elements;++k){
 			KernelPair<dist_t,idx_t> now;
 			if(subtid == 0){
-				now = smmh2::pop_min(q,heap_size[cid]);
+				now = smmh2::pop_min(q[cid],heap_size[cid]);
 				// if(bid==0)
 				// {
 				// 	printf("now:%f,%d\t",now.first,now.second);
 				// 	for(int i=0;i<heap_size[0];i++)
-				// 		printf("i:%d,idx:%d,dst:%f\t",i,q[i].second,q[i].first);
+				// 		printf("i:%d,idx:%d,dst:%f\t",i,q[cid][i].second,q[cid][i].first);
 				// }
-// #ifdef __ENABLE_VISITED_DEL
-// 				pbf->del(now.second);
-// #endif
-				if(k == 0 && topk_heap_size == TOPK && (topk[0].first <= now.first)){
+#ifdef __ENABLE_VISITED_DEL
+				pbf->del(now.second);
+#endif
+				if(k == 0 && topk_heap_size == TOPK && (topk[cid][0].first <= now.first)){
 					++finished[cid];
 				}
 			}
-			__syncthreads();
+			__syncwarp();
+
 			if(finished[cid] >= FINISH_CNT)
 				break;
 			if(subtid == 0){
-				topk[topk_heap_size++] = now;
-				push_heap(topk,topk + topk_heap_size);
-// #ifdef __ENABLE_VISITED_DEL
-// 				pbf->add(now.second);
-// #endif
+				topk[cid][topk_heap_size++] = now;
+				push_heap(topk[cid],topk[cid] + topk_heap_size);
+#ifdef __ENABLE_VISITED_DEL
+				pbf->add(now.second);
+#endif
 				if(topk_heap_size > TOPK){
-// #ifdef __ENABLE_VISITED_DEL
-// 					pbf->del(topk[0].second);
-// #endif
-					pop_heap(topk,topk + topk_heap_size);
+#ifdef __ENABLE_VISITED_DEL
+					pbf->del(topk[cid][0].second);
+#endif			
+					pop_heap(topk[cid],topk[cid] + topk_heap_size);
 					--topk_heap_size;
 				}
 				for(int i = 0;i < degree;++i){
-					auto idx = d_graph[now.second].indexes[i];
-					if(subtid == 0){
-						if(pbf->test(idx)){
-							// if(bid==0)
-							// 	printf("so big%d\n",idx);
-							continue;
-						}
+				auto idx = d_graph[now.second].indexes[i];
+				if(subtid == 0){
+					bool tmp = pbf->test(idx);
+					if(tmp){
+						// if(bid==0)
+						// 	printf("so big%d\n",idx);
+						continue;
+					}
+
 // #ifdef __DISABLE_SELECT_INSERT
+
 // 						pbf->add(idx);
 // #endif
-						index_list[cid][index_list_len[cid]++] = idx;
+					index_list[cid][index_list_len[cid]++] = idx;
 					}
 				}
 			}
 		}
+
 		if(finished[cid] >= FINISH_CNT)
 			break;
-		__syncthreads();
+		__syncwarp();
+
 		
-		int nq=cid;
-		for(int i = 0;i < index_list_len[nq];++i){
+		for(int i = 0;i < index_list_len[cid];++i){
 			value_t tmp = 0;
 			for(int j = subtid;j < dim;j += step){
-				tmp += pq_table[bid+nq][j][d_data[index_list[nq][i] * dim + j]];
+				tmp += (query_point[cid][j] - d_data[index_list[cid][i] * dim + j]) * (query_point[cid][j] - d_data[index_list[cid][i] * dim + j]);
+				//tmp += pq_table[bid+cid][j][d_data[index_list[cid][i] * dim + j]];
 			}
 			for (int offset = 16; offset > 0; offset /= 2){
 				tmp += __shfl_xor_sync(FULL_MASK, tmp, offset);
 			}
 			if(subtid==0){
 				//printf("tmp:%f\n",tmp);
-				dist_list[i] = tmp;
+				dist_list[cid][i] = tmp;
 				//printf("dist_list:%f\n",dist_list[i]);
 			}
 		}
 
-		__syncthreads();
+		__syncwarp();
 
 		if(subtid == 0){
 			for(int i = 0;i < index_list_len[cid];++i){
-				dist_t d = dist_list[i];
+				dist_t d = dist_list[cid][i];
 				KernelPair<dist_t,idx_t> kp;
 				kp.first = d;
 				kp.second = index_list[cid][i];
 
-				if(heap_size[cid] >= QUEUE_SIZE + 1 && q[2].first < kp.first){
+				if(heap_size[cid] >= QUEUE_SIZE + 1 && q[cid][2].first < kp.first){
 					continue;
 				}
 #if N_MULTIPROBE > 1
 				if(pbf->test(kp.second))
 					continue;
-#endif
-				smmh2::insert(q,heap_size[cid],kp);
+#endif			
+				smmh2::insert(q[cid],heap_size[cid],kp);
 #ifndef __DISABLE_SELECT_INSERT
 				pbf->add(kp.second);
 #endif
 				if(heap_size[cid] >= QUEUE_SIZE + 2){
-// #ifdef __ENABLE_VISITED_DEL
-// 					pbf->del(q[2].second);
-// #endif
-					smmh2::pop_max(q,heap_size[cid]);
+#ifdef __ENABLE_VISITED_DEL
+					pbf->del(q[cid][2].second);
+#endif
+                    
+					smmh2::pop_max(q[cid],heap_size[cid]);
+					// clock64();
 				}
 			}
 		}
-		__syncthreads();
-		if(bid+cid==1 && subtid == 0)
-		{
-			//printf("\nquery:%d,step:%d,heapsize:%d\n",bid+cid,step_num,heap_size[cid]);
-			//printf("dist:%f,idx:%d\t",q[1].first,q[1].second);
-			// for(int i=0;i<heap_size[0];i++)
-			// 	printf("i:%d,idx:%d\t",i,q[i].second);
-    	}
+        __syncwarp();
+
+		// if(subtid == 0)
+		// {
+		// 	//d_step_num[bid+cid]=step_num;
+		// 	// printf("\nquery:%d,step:%d,heapsize:%d\n",bid+cid,step_num,heap_size[cid]);
+		// 	//printf("dist:%f,idx:%d\t",q[1].first,q[1].second);
+		// 	// for(int i=0;i<heap_size[0];i++)
+		// 	// 	printf("i:%d,idx:%d\t",i,q[i].second);
+    	// }
 	}
 
 
 	if(subtid == 0){
 		for(int i = 0;i < TOPK;++i){
-			auto now = pop_heap(topk,topk + topk_heap_size - i);
-			d_result[(bid + cid) * TOPK + TOPK - 1 - i] = now.second;
+			auto res = pop_heap(topk[cid],topk[cid] + topk_heap_size - i);
+			d_result[(bid + cid) * TOPK + TOPK - 1 - i] = res.second;
 		}
+
 		//printf("\nquery:%d,pbflen:%d\n",bid+cid,pbf->len);
-		delete[] q;
-		delete[] topk;
+		// delete[] q;
+		// delete[] topk;
     	delete pbf;
     	//delete[] dist_list;
 	}
+
 }
 
 
 static void astar_multi_start_search_batch(const std::vector<std::vector<std::pair<int,value_t>>>& queries,int k,\
-	std::vector<std::vector<idx_t>>& results,pq_idx_t* h_data,graph_node<dim,degree>* h_graph,pq_value_t* pq_centroid,int num){
-	pq_idx_t* d_data;
+	std::vector<std::vector<idx_t>>& results,std::vector<int>& step_num,value_t* h_data,graph_node<dim,degree>* h_graph,pq_value_t* pq_centroid,int num){
+	value_t* d_data;
 	value_t* d_query;
-	idx_t* d_result;
+    idx_t* d_result;
 	pq_value_t* d_pq_centroid;
 	pq_value_t* d_pq_table;
 	value_t* d_datadist_list;
+	int* d_step_num;
 	BLOOMFILTER_DATA_T* d_hashdata;
 
 	graph_node<dim,degree>* d_graph;
-	cudaFuncSetAttribute(warp_independent_search_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, 131072);
+	//cudaFuncSetAttribute(warp_independent_search_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, 131072);
 	
-	cudaMalloc(&d_data,sizeof(pq_idx_t) * num * dim);
+	cudaMalloc(&d_data,sizeof(value_t) * num * dim);
 	cudaMalloc(&d_graph,sizeof(graph_node<dim,degree>) * num);
 	cudaMalloc(&d_pq_centroid,sizeof(pq_value_t) * 256 * dim);
-	cudaMemcpy(d_data,h_data,sizeof(pq_idx_t) * num * dim,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_data,h_data,sizeof(value_t) * num * dim,cudaMemcpyHostToDevice);
 	cudaMemcpy(d_graph,h_graph,sizeof(graph_node<dim,degree>) * num,cudaMemcpyHostToDevice);
 	cudaMemcpy(d_pq_centroid,pq_centroid,sizeof(pq_value_t) * 256 * dim,cudaMemcpyHostToDevice);
 
@@ -349,80 +383,80 @@ static void astar_multi_start_search_batch(const std::vector<std::vector<std::pa
 		}
 	}
 	std::unique_ptr<idx_t[]> h_result = std::unique_ptr<idx_t[]>(new idx_t[queries.size() * TOPK]);
+	int h_step_num[queries.size()];
 
 	cudaMalloc(&d_query,sizeof(value_t) * queries.size() * dim);
-	cudaMalloc(&d_result,sizeof(idx_t) * queries.size() * TOPK);
+    cudaMalloc(&d_result,sizeof(idx_t) * queries.size() * TOPK);
 	cudaMalloc(&d_pq_table,sizeof(pq_value_t) * pq_dim * 256 * queries.size());
-	cudaMalloc(&d_datadist_list,sizeof(value_t) * queries.size() * FIXED_DEGREE * N_MULTIPROBE);
+	cudaMalloc(&d_datadist_list,sizeof(value_t) * queries.size() * (FIXED_DEGREE * N_MULTIPROBE));
 	cudaMalloc(&d_hashdata,sizeof(BLOOMFILTER_DATA_T)*queries.size()* BLOOM_FILTER_BIT64 * BLOOMFILTER_SIZE64MULT);
+	cudaMalloc(&d_step_num,sizeof(int) * queries.size());
 	
+
 	cudaMemcpy(d_query,h_query.get(),sizeof(value_t) * queries.size() * dim,cudaMemcpyHostToDevice);
 
 
 	// warp_independent_search_kernel<<<queries.size()/N_MULTIQUERY,32>>>(d_data,d_query,d_result,d_graph,d_pq_centroid,queries.size());
+
 	warp_independent_search_kernel<<<queries.size()/N_MULTIQUERY,N_THREAD_IN_BLOCK>>>\
 	(d_data,d_query,d_result,d_graph,d_pq_centroid,d_pq_table,d_datadist_list,d_hashdata,queries.size());
-
 	cudaDeviceSynchronize();
 
-	cudaMemcpy(h_result.get(),d_result,sizeof(idx_t) * queries.size() * TOPK,cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_result.get(),d_result,sizeof(idx_t) * queries.size() * TOPK,cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_step_num,d_step_num,sizeof(int) * queries.size(),cudaMemcpyDeviceToHost);
 
-	results.clear();
+    results.clear();
 	for(int i = 0;i < queries.size();++i){
 		std::vector<idx_t> v(TOPK);
 		for(int j = 0;j < TOPK;++j)
 			v[j] = h_result[i * TOPK + j];
 		results.push_back(v);
 	}
+
+
 	cudaFree(d_data);
 	cudaFree(d_query);
-	cudaFree(d_result);
+    cudaFree(d_result);
 	cudaFree(d_graph);
 	cudaFree(d_pq_centroid);
 	cudaFree(d_pq_table);
 	cudaFree(d_hashdata);
 	cudaFree(d_datadist_list);
+	cudaFree(d_step_num);
 }
 
 
 int main() {
 	cudaSetDevice(0);
 
-    // 内存中保存pq量化数据
-    // pq_idx_t* h_data = new pq_idx_t[num_vertices * pq_dim];
-    // for (int i = 0; i < num_vertices * pq_dim; ++i) {
-    //     h_data[i] = static_cast<pq_idx_t>(rand());
-    // }
 	u_int64_t npts_u64, nchunks_u64;
-    pq_idx_t *h_data = nullptr;
-	load_bin<pq_idx_t>("/home/xy/anns-2/ann_search/mini_graph/disk_index_sift_learn_R64_L128_A1.2_pq_compressed.bin", h_data, npts_u64, nchunks_u64);
+    // // 读取pq量化后向量数据
+    // pq_idx_t *h_data = nullptr;
+	// load_bin<pq_idx_t>("/home/xy/anns-2/ann_search/mini_graph/disk_index_sift_learn_R64_L128_A1.2_pq_compressed.bin", h_data, npts_u64, nchunks_u64);
 
-    
-    // vamana图结构，包含完整向量和索引
-    // graph_node<dim,degree>* h_graph = new graph_node<dim,degree>[num_vertices];
-    // for (int i = 0; i < num_vertices; ++i) {
-    //     for (int j = 0; j < dim; ++j)
-    //         h_graph[i].values[j] = static_cast<value_t>(rand()); // Random vertex index
-    //     for (int j = 0; j < degree; ++j)
-    //         h_graph[i].indexes[j] = static_cast<idx_t>(rand()) % num_vertices;
-    // }
+    // 读取未量化向量数据
+	value_t* h_data=new value_t[num_vertices*dim];
+	std::ifstream file("/home/xy/anns-2/ann_search/mini_graph/bfsg.data", std::ios::binary | std::ios::ate);
+	if (!file.is_open()) {
+        std::cerr << "无法打开文件: " << "bfsg.data" << std::endl;
+    }
+
+	std::streamsize size = file.tellg();
+	file.seekg(0, std::ios::beg);
+	
+
+	if (!file.read(reinterpret_cast<char*>(h_data), size)) {
+        std::cerr << "读取文件失败: " << "bfsg.data" << std::endl;
+        delete[] h_data;
+    }
+
+    // 读取图结构向量，在SSD版本可移除。
+
 	std::vector<graph_node<dim,degree>> nodes;
 	read_node_bin("/home/xy/anns-2/ann_search/mini_graph/disk_index_sift_learn_R64_L128_A1.2_disk.index", nodes);
 	graph_node<dim,degree>* h_graph=nodes.data();
-	// for(int i=0;i<degree;i++)
-	// {
-	// 	std::cout<<"indexs:"<<static_cast<unsigned>(h_graph[0].indexes[i])<<std::endl;
-	// }
 
-
-    // 查询
-    // std::vector<std::vector<std::pair<int,value_t>>> queries(num_queries);
-    // for (int i = 0; i < num_queries; ++i) {
-    //     // Each query has random values for different dimensions
-    //     for (int j = 0; j < dim; ++j) {
-    //         queries[i].push_back(std::make_pair(j, static_cast<value_t>(rand())));
-    //     }
-    // }
+    // 读取查询向量
 
 	float *query = nullptr;
 	load_bin<value_t>("/home/xy/anns-2/ann_search/mini_graph/sift_query.fbin", query, npts_u64, nchunks_u64);
@@ -433,15 +467,10 @@ int main() {
         }
     }
 	
-	
 
 
-    // pq中心表
 
-    // pq_value_t* pq_centroid=new pq_value_t[256 * dim];
-	// for (int i = 0; i < 256 * dim; ++i) {
-	// 	pq_centroid[i] = static_cast<pq_value_t>(rand());
-	// }
+    // 读取pq中心表
 
 	uint32_t *chunk_offsets = nullptr;
     float *centroid = nullptr;
@@ -486,22 +515,27 @@ int main() {
 
     // 结果保存
     std::vector<std::vector<idx_t>> results;
+	std::vector<int> step_num;
 
     // Call the function
-    astar_multi_start_search_batch(queries, TOPK, results, h_data, h_graph, pq_centroid, num_vertices);
+	
+    astar_multi_start_search_batch(queries, TOPK, results, step_num, h_data, h_graph, pq_centroid, num_vertices);
 
 	saveToCSV(results, "/home/xy/anns-2/ann_search/results.csv");
 
-    // 结果输出
-    std::cout << "Results:" << std::endl;
-    for (int i = 0; i < results.size(); ++i) {
-        std::cout << "Query " << i << ":" << std::endl;
-        for (int j = 0; j < results[i].size(); ++j) {
-            std::cout << ' '<<results[i][j] << ",";
-        }
-        std::cout << std::endl;
-    }
+    // // 结果输出
+    // std::cout << "Results:" << std::endl;
+    // for (int i = 0; i < results.size(); ++i) {
+    //     std::cout << "Query " << i << ":" << std::endl;
+    //     for (int j = 0; j < results[i].size(); ++j) {
+    //         std::cout << ' '<<results[i][j] << ",";
+    //     }
+    //     std::cout << std::endl;
+    // }
 
-
+    // int sum = std::accumulate(step_num.begin(), step_num.end(), 0);
+    // double average = static_cast<double>(sum) / step_num.size();
+    // std::cout << "average step_num: " << average << std::endl;
+	// std::cout<<std::endl;
     return 0;
 }
